e3f94db12df4c28fd0c93cfa76fe2db982e987aa
Claire Alverson <clairetalverson@gmail.com>
2024-06-06 21:34:15 -0500

    Added temp photos and import and export file.

--- client/src/pages/UserPlants.js
+++ client/src/pages/UserPlants.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import * as tempImages from '../assets/images/index';
 
+console.log(tempImages.seaoats)
 function UserPlants() {
     return (
         <div>
             ONCE SIGNED IN, a user can see:
             <ul>
                 <li>
+                    <img src={tempImages.seaoats} />
                     Their plants that they have scanned. Sorted by their yard, other locations, native to home location, native to region, non native.
                 </li>
                 <li>
